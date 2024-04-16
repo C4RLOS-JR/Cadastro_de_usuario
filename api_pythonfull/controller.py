e9de7145ac60a8b8d62dcae7e40d713dae0f0a68
Leonam-Comercial <leonam.comseg@gmail.com>
2024-04-16 16:25:20 -0300

    Leonam

--- api_pythonfull/controller.py
+++ api_pythonfull/controller.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 # cors fastapi (policy) → https://fastapi.tiangolo.com/tutorial/cors/
 # origins = ['*'] # '*' → todos podem fazer requisição
-origins = ['http://127.0.0.1:5001', 'http://127.0.0.1:5002']
+origins = ['http://127.0.0.1:5001', 'http://127.0.0.1:5002', 'http://127.0.0.1:5003']
 
 app.add_middleware(
     CORSMiddleware,
