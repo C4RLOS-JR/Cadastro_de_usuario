3dbefbd104db5161395266b22526389b803567ae
Leonam-Comercial <leonam.comseg@gmail.com>
2024-04-17 17:01:31 -0300

    Leonam

--- aula_api/server.py
+++ aula_api/server.py
@@ -1,9 +1,14 @@
 def app(amb, start_response):
-  arq = open('index.html', 'rb')
-  data = arq.read()
+  cadastro = open('cadastro.html', 'rb')
+  login = open('login.html', 'rb')
+  if 'logar' in amb['PATH_INFO']:
+    data = login.read()
+  else:
+    data = cadastro.read()
   status = '200 ok'
   response_headers = [('Content-type', 'text/html')]
   start_response(status, response_headers)
+    
   return [data]
 
 # gunicorn server:app -b 127.0.0.1:5002
